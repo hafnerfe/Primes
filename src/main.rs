0c440f6d3c9289dcd39fa8647a4e538d4176dc61
hafnerfe <felixhafner2161997@googlemail.com>
2019-08-15 19:55:55 +0200

    added functionality: find a big prime

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use rand::Rng;
+// TODO: get rid of casting.
 
 fn main() {
- for i in 3..500 {
-  println!("{}: {}", i, is_prime(i, 100));
+ print_primes_range(50, 100);
+ println!("{} is a big prime", get_big_prime(61, 100));
+
+}
+
+fn print_primes_range(start: u64, end: u64) {
+ for i in start..end {
+  if is_prime(i, 200) { println!("{} is prime", i); }
  }
 }
 
 fn is_prime(m:u64, n_runs:usize) -> bool {
  // m is prime -> return true
- // m is composite -> return false with probability >= 0.5^(-n_runs)
+ // m is composite -> return false with probability >= 1 - 2^(-n_runs)
  std::iter::repeat(m).take(n_runs).map(probably_prime).all(|x| x)
 }
 
@@ -23,24 +30,37 @@ fn probably_prime(m: u64) -> bool {
   l = l / 2;
  }
  let mut x = rand::thread_rng().gen_range(1, m);
- x = square_multiply(x, l, m);
- //x = 1;
- // x = u64::pow(x, l) % m; // alternative
+ x = square_multiply(x as u128, l as u128, m as u128);
  i = i - 1;
  let mut y = x;
- x = square_multiply(x, 2, m);
+ x = square_multiply(x as u128, 2u128, m as u128);
  while x != 1 && i >= 0 {
   i = i - 1;
   y = x;
-  x = square_multiply(x, 2, m);
+  x = square_multiply(x as u128, 2u128,  m as u128);
  }
  i >= 0 && 1 <= y && y <= m - 1
 }
 
-fn square_multiply(base: u64, pow: u64, modulo: u64) -> u64 {
+fn square_multiply(base: u128, pow: u128, modulo: u128) -> u64 {
  if pow == 0 {1}
- else if pow == 1 {base % modulo}
+ else if pow == 1 {(base % modulo) as u64}
  else if pow % 2 == 0 {square_multiply((base * base) % modulo, pow / 2, modulo)}
- else {(base * square_multiply((base * base) % modulo, (pow-1) / 2, modulo)) % modulo}
+ else {((base * square_multiply((base * base) % modulo, (pow - 1) / 2, modulo) as u128) % modulo) as u64}
+}
 
+fn get_big_number(n_bits: u32) -> u64 {
+ let rand_number : u64 = rand::random::<u64>();
+ let bitmask : u64 = 2u64.pow(n_bits) - 1;
+ (rand_number & bitmask) | 2u64.pow(n_bits - 1)
+}
+
+fn make_uneven(number: u64) -> u64 {
+ number | 1
+}
+
+fn get_big_prime(n_bits: u32, n_runs: usize) -> u64 {
+ let possible_primes = std::iter::repeat(n_bits).map(get_big_number).map(make_uneven);
+ let index = possible_primes.clone().map(|p| is_prime(p, n_runs)).position(|x| x).unwrap();
+ possible_primes.clone().nth(index).unwrap()
 }
